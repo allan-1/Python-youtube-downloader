86e0f8792d279578609866665d810f6b9465fdca
Allan Muturi <amuturi01@gmail.com>
2021-06-26 12:27:23 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import pytube
+import os
+
+def downloadYouTube(videourl, path):
+
+    yt = pytube.YouTube(videourl)
+    yt = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
+    if not os.path.exists(path):
+        os.makedirs(path)
+    yt.download(path)
+
+video = input("Enter the videos url : ")
+save_path = input("Enter the path to save : ")
+
+downloadYouTube(video, save_path)
